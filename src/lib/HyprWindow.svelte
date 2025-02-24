b3286030746d77f2e7113f15cdb2b1943777c891
anal <hanbiao4079@gmail.com>
2025-02-24 21:35:03 +0800

    init

--- src/lib/HyprWindow.svelte
+++ src/lib/HyprWindow.svelte
@@ -3,25 +3,103 @@
   import View from "./View.svelte";
 
   let { appWindow }: { appWindow: AppWindow } = $props();
+  let resizeRef: HTMLDivElement;
+  let containerRef: HTMLDivElement;
+  let isResizing = $state(false);
 
-  let gridTemplateColumns = $state("1fr");
-
-  $effect(() => {
-    const length = appWindow.children.length;
-    let columns = [1];
-    for (let i = 1; i < length; i++) {
-      const last = columns[columns.length - 1];
-      columns = [...columns.slice(0, -1), last / 2, last / 2];
+  let masterView = $derived(()=>{
+    if(appWindow.children.length > 0) {
+      return appWindow.children[0]
     }
-    gridTemplateColumns = columns.map((c) => `${c}fr`).join(" ");
-  });
+  })
+
+  let stackView = $derived(()=>{
+    if(appWindow.children.length > 1) {
+      return appWindow.children.slice(1)
+    }
+    return []
+  })
+
+  $effect(()=>{
+
+    isResizing = false
+
+    const container = document.querySelector('.container');
+
+    resizeRef.addEventListener('mousedown', () => {
+      isResizing = true;
+      document.body.style.cursor = 'col-resize';
+    });
+
+    document.addEventListener('mousemove', (e) => {
+      if (!isResizing) return;
+
+      const containerRect = containerRef.getBoundingClientRect();
+      console.log(e.clientX, containerRect.left)
+
+
+      const newWidth = (e.clientX - containerRect.left) / containerRect.width * 100;
+
+      document.documentElement.style.setProperty(
+        '--master-width',
+        `${Math.min(Math.max(20, newWidth), 80)}%`
+      );
+    });
+
+    document.addEventListener('mouseup', () => {
+      isResizing = false;
+      document.body.style.cursor = 'default';
+    });
+  })
+
+
 </script>
 
 <div
-  class="window w-full h-full grid grid-flow-col gap-4"
-  style="grid-template-columns: {gridTemplateColumns}; grid-auto-flow: column;"
+  bind:this={containerRef}
+  class="window flex w-full h-full"
 >
-  {#each appWindow.children as item, i}
-    <View view={item}></View>
-  {/each}
+  <div class="master-area">
+      <View view={masterView()}></View>
+  </div>
+  <div bind:this={resizeRef} class="resize-handle"></div>
+  <div class="stack-area flex-1 flex flex-column">
+    {#each stackView() as item, i}
+      <View view={item}></View>
+    {/each}
+  </div>
 </div>
+
+<style>
+
+:root {
+  --gap: 5px;
+  --master-width: 60%;
+}
+
+.window {
+  gap: var(--gap);
+  padding: var(--gap);
+}
+.master-area {
+  flex: 1 1 var(--master-width);
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap);
+}
+
+.stack-area {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap);
+}
+
+.resize-handle {
+  width: 5px;
+  background: #4c566a;
+  cursor: col-resize;
+}
+
+
+</style>
--- src/lib/View.svelte
+++ src/lib/View.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
   import { activeView, type ViewApp } from "../store/app.store";
 
-  const { view }: { view: ViewApp } = $props();
+  const { view }: { view?: ViewApp } = $props();
 
   const handleMouseenter = () => {
-    if ($activeView?.viewId !== view.viewId) {
+    if ($activeView?.viewId !== view?.viewId && view) {
       activeView.set(view);
     }
   };
 </script>
 
-<div
-  role="application"
-  onmouseenter={handleMouseenter}
-  class="view flex-1 rounded-md border-2 border-transparent hover:border-amber-500"
->
-  <view.element></view.element>
-</div>
+{#if view}
+  <div
+    role="application"
+    onmouseenter={handleMouseenter}
+    class="view flex-1 rounded-md border-2 border-transparent hover:border-amber-500"
+  >
+    <view.element></view.element>
+  </div>
+{/if}
